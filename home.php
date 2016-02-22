52a8310657235ccbbb1a06993b6dec8fc87dde64
OWASP BWA <root@brokenwebapps.localdomain>
2016-02-22 15:30:37 -0500

    asdf

--- home.php
+++ home.php
@@ -6,7 +6,7 @@
 <p>Smertpeepler.us is for smert peeple like you and me. 
 <br>Get to no other peeple who r smert. Show how smert u r!!!</p>
 
-<a href="smertpeepler.us/smertpeepler.us/home.php">Home</a><br>
+<a href="home.php">Home</a><br>
 <a href="smertpeepler.us/smertpeepler.us/login.php">Login</a><br>
 <a href="smertpeepler.us/smertpeepler.us/register.php">Register</a><br>
 
