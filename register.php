af7fb38ee256fc9145cd7d2377093860ee1dd7ed
OWASP BWA <root@brokenwebapps.localdomain>
2016-02-22 15:19:54 -0500

    asdhf

---
+++ register.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+<h1>Register</h1>
+
+<p></p>
+
+<a href="192.168.183.128/smertpeepler.us/smertpeepler.us/home.php">Home</a><br>
+<a href="192.168.183.128/smertpeepler.us/smertpeepler.us/login.php">Login</a><br>
+<a href="192.168.183.128/smertpeepler.us/smertpeepler.us/register.php">Register</a><br>
+
+
+</body>
+</html>
+
+
